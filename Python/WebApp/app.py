b3fcb43c11e486f62dcf46280cfae80566dccaaf
TobyBBrown <tbrown08@dcs.bbk.ac.uk>
2018-08-16 13:43:45 +0100

    Correctly moved sql models to separate file, added order by option

--- Python/WebApp/app.py
+++ Python/WebApp/app.py
@@ -2,46 +2,12 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 from regex import *
+from models import *
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Katm2803@localhost:3306/test'
-app.config['SQLALCHEMY_BINDS'] = {
-    'project' : 'mysql+mysqlconnector://root:Katm2803@localhost:3306/project'}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Katm2803@localhost:3306/project'
 db = SQLAlchemy(app)
-
-
-class test(db.Model):
-    appid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    min_specs = db.Column(db.Text)
-    rec_specs = db.Column(db.Text)
-
-
-class game_requirements(db.Model):
-    __bind_key__ = 'project'
-    appid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    min_specs = db.Column(db.Text)
-    rec_specs = db.Column(db.Text)
-    min_cpu_score = db.Column(db.Integer)
-    rec_cpu_score = db.Column(db.Integer)
-    min_gpu_score = db.Column(db.Integer)
-    rec_gpu_score = db.Column(db.Integer)
-    userscore = db.Column(db.Integer)
-    price = db.Column(db.Integer)
-
-
-class cpubenchmarks(db.Model):
-    __bind_key__ = 'project'
-    cpu_name = db.Column(db.String, primary_key=True)
-    benchmark_score = db.Column(db.Integer)
-
-
-class gpubenchmarks(db.Model):
-    __bind_key__ = 'project'
-    gpu_name = db.Column(db.String, primary_key=True)
-    benchmark_score = db.Column(db.Integer)
 
 
 store_url = 'https://store.steampowered.com/app/'
@@ -49,19 +15,20 @@ store_url = 'https://store.steampowered.com/app/'
 
 @app.route("/input")
 def hardware_input():
-    return render_template('input.html', cpus=cpubenchmarks.query.all(),
-                           gpus=gpubenchmarks.query.all())
+    return render_template('input.html', cpus=db.session.query(cpubenchmarks).all(),
+                           gpus=db.session.query(gpubenchmarks).all())
 
 
 @app.route("/hardware_submit", methods=['POST'])
 def hardware_submit():
-    cpu = cpubenchmarks.query.get(request.form['CPU'])
-    gpu = gpubenchmarks.query.get(request.form['GPU'])
+    cpu = db.session.query(cpubenchmarks).get(request.form['CPU'])
+    gpu = db.session.query(gpubenchmarks).get(request.form['GPU'])
     ram = int(request.form['ram_num'])
     os = int(request.form['os_version'])
-    rows = game_requirements.query.filter(game_requirements.min_cpu_score < cpu.benchmark_score,
+    order = request.form['order_by']
+    rows = db.session.query(game_requirements).filter(game_requirements.min_cpu_score < cpu.benchmark_score,
                                           game_requirements.min_gpu_score < gpu.benchmark_score).order_by\
-                                          (desc(game_requirements.userscore)).all()
+                                          (desc(order)).all()
     os_ram_rows = []
     for row in rows:
         if (os_regex(row.min_specs) is None or os >= os_regex(row.min_specs)) and \
--- Python/WebApp/models.py
+++ Python/WebApp/models.py
@@ -5,19 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class test(Base):
-    __tablename__ = 'test'
-
-    appid = Column(Integer, primary_key=True)
-    name = Column(String)
-    min_specs = Column(Text)
-    rec_specs = Column(Text)
-
-
 class game_requirements(Base):
     __tablename__ = 'game_requirements'
 
-    __bind_key__ = 'project'
     appid = Column(Integer, primary_key=True)
     name = Column(String)
     min_specs = Column(Text)
@@ -25,13 +15,14 @@ class game_requirements(Base):
     min_cpu_score = Column(Integer)
     rec_cpu_score = Column(Integer)
     min_gpu_score = Column(Integer)
-    _rec_gpu_score = Column(Integer)
+    rec_gpu_score = Column(Integer)
+    userscore = Column(Integer)
+    price = Column(Integer)
 
 
 class cpubenchmarks(Base):
     __tablename__ = 'cpubenchmarks'
 
-    __bind_key__ = 'project'
     cpu_name = Column(String, primary_key=True)
     benchmark_score = Column(Integer)
 
